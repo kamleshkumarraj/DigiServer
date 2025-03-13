d5ed3805654d9ddb08e3ba4787b42c110a132505
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-03-13 18:27:44 +0530

    we create schema for collage and university also

---
+++ src/models/collage.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const collageSchema = new Schema({
+    collageName : {
+        type : String,
+        required : true,
+    },
+    registrationId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    country : {
+        type : String,
+        required : true
+    },
+    state : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    postalCode : {
+        type : Number,
+        required : true
+    },
+    universityId : {
+        type : Schema.Types.ObjectId,
+        ref : "University",
+        required : true
+    }
+})
+
+export const Collage = mongoose.model("Collage", collageSchema);
---
+++ src/models/university.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const universitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique : true
+    },
+    registrationId: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state :{
+        type : String,
+        required : true
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    postalCode : {
+        type : Number,
+        required : true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image : {
+        public_id : {
+            type : String,
+            required : true
+        },
+        url : {
+            type : String,
+            required : true
+        }
+    }
+})
+
+export const University = mongoose.model("University", universitySchema);

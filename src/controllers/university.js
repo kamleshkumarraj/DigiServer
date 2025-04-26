17819419f7bf5868d7b6165f820cd3a862b1bcdc
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-04-26 23:41:44 +0530

    We write the controller for updating university

--- src/controllers/university.js
+++ src/controllers/university.js
@@ -1,3 +1,4 @@
+import mongoose, { mongo } from "mongoose";
 import { asyncErrorHandler } from "../errors/asynError";
 import { ErrorHandler } from "../errors/errorHandler";
 import { University } from "../models/university.model";
@@ -22,4 +23,21 @@ export const createUniversity = asyncErrorHandler(async (req, next, next) => {
 })
 
 // now we write code for update university.
-export const updateUniversity = asyncErrorHandler
+export const updateUniversity = asyncErrorHandler(async (req, res, next) => {
+    const data = req.body;
+    const id = req.params.id;
+    if(!mongoose.isValidObjectId(id)) return next(new ErrorHandler("Invalid University Id",400));
+
+    const university = await University.findOne({ registrationId : data.registrationId });
+
+    if(!university) return next(new ErrorHandler("University not found !",404));
+
+    const newUniversity = University.findOneAndUpdate({ registrationId : data.registrationId }, data, { new : true });
+
+    res.status(200).json({
+        success : true, 
+        message : "University updated successfully",
+        data : newUniversity
+    })
+
+})

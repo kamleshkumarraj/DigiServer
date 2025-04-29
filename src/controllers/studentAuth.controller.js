d40d9990e80a312714a0f2158af433df158da59c
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-04-29 20:30:03 +0530

    We write the controller for logout the  user

--- src/controllers/studentAuth.controller.js
+++ src/controllers/studentAuth.controller.js
@@ -62,3 +62,14 @@ export const login = asyncErrorHandler(async (req, res, next) => {
   // if password is correct then we login the student.
   loginWithJWT(student, res);
 })
+
+// now we write controller for logout student.
+export const logout = asyncErrorHandler(async (req, res, next) => {
+  res.clearCookie('token');
+  req.user = undefined;
+  res.cookie('token',undefined).status(200).json({
+    success: true,
+    message: "User logged out successfully."
+  })
+})
+

31baa31208b2713eb9d20562b017505574317130
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-06-07 10:28:29 +0530

    we write the code for login page

--- src/routes/university.routes.js
+++ src/routes/university.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createUniversity, updateUniversity, updateUniversityId, updateUniversityImage } from "../controllers/university.controller.js";
+import { upload } from "../middlewares/uploadFile.js";
 
 export const universityRouter = Router();
 
-universityRouter.route("/create-university").post(uplo,createUniversity);
+universityRouter.route("/create-university").post(upload.single('image'),createUniversity);
 universityRouter.route("/update-university/:id").put(updateUniversity);
 universityRouter.route("/update-university-id/:id").patch(updateUniversityId);
 universityRouter.route("/update-university-image/:id").patch(updateUniversityImage);

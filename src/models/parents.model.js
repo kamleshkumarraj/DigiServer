4304b64eebad814d6e4a0859dc34db6e31650fcf
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-08-19 17:25:58 +0530

    we create parent model

---
+++ src/models/parents.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const parentSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : [true, "First name is required"],
+        trim : true,
+        max : [50 , "First name must be less than 50 characters"],
+        min : [3, "First name must be more than 3 characters"]
+    },
+    lastName : {
+        type : String,
+        required : [true, "Last name is required"],
+        trim : true,
+        max : [50 , "Last name must be less than 50 characters"],
+        min : [3, "Last name must be more than 3 characters"]
+    },
+    phoneNumber : {
+        type : String,
+        required : [true, "Phone number is required"],
+        trim : true,
+        max : [10, "Phone number must be less than 15 characters"],
+        min : [10, "Phone number must be more than 10 characters"]
+    },
+    gender : {
+        type : String,
+        required : [true, "Gender is required"],
+        enum : ['male', 'female', 'custom']
+    },
+    contactDetails : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Contact"
+    },
+    age : {
+        type : Number,
+        required : [true, "Age is required"],
+        min : [1, "Age must be more than 0"],
+        max : [100, "Age must be less than 100"]
+    },
+    bio : {
+        type : String,
+        required : [true, "Bio is required"],
+        trim : true,
+        max : [500 , "Bio must be less than 500 characters"],
+        min : [3, "Bio must be more than 3 characters"]
+    },
+    child : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : [true, "Child is required"]
+    },
+    collageId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "College"
+    },
+    universityId : { 
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "University"
+    }
+
+})
+
+export const Parent = mongoose.model("Parent", parentSchema);

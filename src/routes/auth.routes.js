3f245767b0099b588fd77b53ccc38fdaabfcf59a
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-08-20 22:46:30 +0530

    we add route for direct login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
-import { login, logout, register, updateAvatar } from "../controllers/auth.controller.js";
+import { directLogin, login, logout, register, updateAvatar } from "../controllers/auth.controller.js";
 import { upload } from "../middlewares/uploadFile.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 export const authRouter = Router();
 
 authRouter.route("/register").post(upload.single("avatar"), register);
 authRouter.route("/login").post(login);
 authRouter.route("/logout").post(logout);
-authRouter.route("/update-avatar").patch(updateAvatar);
+authRouter.route("/update-avatar").patch(isLoggedIn,updateAvatar);
+authRouter.route("/direct-login").post(isLoggedIn, directLogin);

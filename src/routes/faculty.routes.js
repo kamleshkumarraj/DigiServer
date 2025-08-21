f25b93537a7ee422fc176c3af3f4889b3b7ead7d
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-08-21 17:16:05 +0530

    we write routes for getting all semester and branch

--- src/routes/faculty.routes.js
+++ src/routes/faculty.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { isFaculty, isLoggedIn } from '../middlewares/auth.middleware.js';
-import { getFacultyProfile } from '../controllers/faculty.controller.js';
+import { getFacultyProfile, getMyBranches, getSemesterForBranch } from '../controllers/faculty.controller.js';
 
 export const facultyRouters = Router();
 
 facultyRouters.route('/my-profile').get(isLoggedIn, isFaculty,getFacultyProfile);
+facultyRouters.route('/my-branch').get(isLoggedIn, isFaculty, getMyBranches);
+facultyRouters.route('/my-semester/:branchId').get(isLoggedIn, isFaculty, getSemesterForBranch);

64ac3eb5a7433c2fc4307160de14e904369831fc
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-08-22 16:56:58 +0530

    we modify batch models

--- src/models/batch.models.js
+++ src/models/batch.models.js
@@ -1,16 +1,25 @@
 import mongoose from "mongoose";
 
-const batchSchema  = new mongoose.Schema({
-    batchName : {
-        type : String,
-        required : [true, "Batch name is required"],
+const batchSchema = new mongoose.Schema(
+  {
+    batchName: {
+      type: String,
+      required: [true, "Batch name is required"],
     },
-    classRoomId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Classroom",
-        required : true
-    }
-}, {timestamps : true});
+    classRoomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Classroom",
+      required: true,
+    },
+    faculty: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Faculty",
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Batch = mongoose.model("Batch", batchSchema);
-
